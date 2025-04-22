2e8cf3c4d93aac930579b9bf79198c4c89c807f4
SamuelRodriguesMGR <marat78880@gmail.com>
2025-04-22 12:35:05 +0500

    упрощение

--- Scripts/MaratEngine/utils/Node.py
+++ Scripts/MaratEngine/utils/Node.py
@@ -12,13 +12,12 @@ class Node:
         pass
 
 class Node2D(Node):
-    def __init__(self, screen, x : int = 0, y : float = 0, scale : float = 1.0, angle : float = 0.0) -> None:
+    def __init__(self, screen, x : int = 0, y : float = 0, scale : float = 1.0) -> None:
         self.screen = screen
         self.next = None
         self.x : int = x
         self.y : int = y
         self.scale : float = scale
-        self.angle : float = angle
 
     def draw() -> None:
         pass
@@ -26,6 +25,9 @@ class Node2D(Node):
     def _process(self) -> None:
         pass
 
+
+
+# Фигуры
 
 class Shapes(Node2D):
     def __init__(self, screen, x = 0, y = 0, scale = 1):
@@ -42,10 +44,14 @@ class Circle(Shapes):
         pygame.draw.circle(self.screen, self.color, (self.x, self.y), self.size)
 
 
+
+
+# Картинки
+
 class Sprite(pygame.sprite.Sprite, Node2D):
     def __init__(self, image_path, screen, x : int = 0, y : float = 0, scale : float = 1.0, angle : float = 0.0):
         pygame.sprite.Sprite.__init__(self)  # Инициализация спрайта
-        Node2D.__init__(self, screen, x, y, scale, angle)  # Инициализация Node2D
+        Node2D.__init__(self, screen, x, y, scale)  # Инициализация Node2D
 
         # Загружаем изображение
         self.image = pygame.image.load(image_path).convert_alpha()
@@ -58,12 +64,13 @@ class Sprite(pygame.sprite.Sprite, Node2D):
         )
 
         # Вращение
+        self.angle : float = angle
         self.image = pygame.transform.rotate(self.image, -self.angle)
 
         # Получаем прямоугольник изображения
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
+        self.surface = self.image.get_rect()
+        self.surface.x = x
+        self.surface.y = y
         
     def draw(self) -> None:
-        self.screen.blit(self.image, self.rect)
+        self.screen.blit(self.image, self.surface)
